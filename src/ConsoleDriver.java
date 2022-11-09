92f90a0583ded2a53c6ac697816e437d3eea8dc8
NEWCOMBDE21 <NewcombDE21@GCC.edu>
2022-11-09 13:35:34 -0500

    New Repository for Group Project

---
+++ src/ConsoleDriver.java
@@ -0,0 +1,16 @@
+public class ConsoleDriver {
+    public static void main(String[] args) {
+        //bulk of logic elsewhere
+        //should only have input-output logic
+        //such as: talk to user, get user input, show changes to user
+
+        //eg
+        //create am object of type Game, Adventure...
+        //Game g = new Game();
+        //maybe while(g.hasWon())
+        //ask user for input
+        //call g.displayStatus() --- or displayBoard()
+
+        //can add a GUI with minimal if any changes to game logic
+    }
+}
