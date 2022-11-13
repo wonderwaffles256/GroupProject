1146e65172e639cb039cb4591e03599361b23504
MALENCIASM21 <smalencia99@gmail.com>
2022-11-12 20:56:45 -0500

    Chest room

--- src/ChestRoom.java
+++ src/ChestRoom.java
@@ -1,3 +1,12 @@
 public class ChestRoom extends Location{
+    Item It;
 
+    public ChestRoom(String name,int difficulty,Item it){
+        super(name,difficulty);
+        It = it;
+    }
+
+    public Item acquireItem(){
+        return It;
+    }
 }
