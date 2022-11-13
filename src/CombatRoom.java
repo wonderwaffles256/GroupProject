c5074aae8dbe7d795783bef5eaaeb9f14c887d2b
NEWCOMBDE21 <NewcombDE21@GCC.edu>
2022-11-12 20:57:07 -0500

    added more to location and combat room

--- src/CombatRoom.java
+++ src/CombatRoom.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class CombatRoom extends Location{
+    ArrayList<Enemy> enemies;
+
+    public CombatRoom(String name, int difficulty, int currentLoc, int progress, ArrayList<Enemy> enemies) {
+        super(name, difficulty, currentLoc, progress);
+
+        this.enemies = new ArrayList<>();
+        this.enemies.addAll(enemies);
+    }
+
+
 
 }
