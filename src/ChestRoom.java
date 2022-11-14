3558b9e5f1d89319c548ce883d782f98cfe69975
MALENCIASM21 <smalencia99@gmail.com>
2022-11-14 16:56:41 -0500

    commented out character and chestroom with javadoc

--- src/ChestRoom.java
+++ src/ChestRoom.java
@@ -1,5 +1,8 @@
 import java.util.Random;
 
+/**
+ * Chest room is a subclass of a room that rewards the player with Corn from a chest in the room
+ */
 public class ChestRoom extends Room{
 
     //constructor given parameters
@@ -7,9 +10,13 @@ public class ChestRoom extends Room{
         super(name);
     }
 
-    //returns some number of CORN from the chest
+    /**
+     * openChest jst gives the player a random amount of corn from the chest to spend later in the shop
+     * @return - the amount of corn earned from a chest
+     */
     public int openChest(){
         int va = 50;//val will be some random value between # and #
+        System.out.println("You opened a chest and got " + va + " amount of CORN");
         return va;
     }
 }
