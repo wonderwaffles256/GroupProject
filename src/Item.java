0c00c125b5130d8b896e3ddde2b386cad0eb11c5
NEWCOMBDE21 <NewcombDE21@GCC.edu>
2022-11-17 16:38:48 -0500

    added load method to RunGame and item description variable to Item

--- src/Item.java
+++ src/Item.java
@@ -6,6 +6,7 @@
 public class Item {
     private String name;
     private int value;
+    private String description;
 
     /**
      * default constructor
@@ -13,6 +14,7 @@ public class Item {
     public Item() {
         name = null;
         value = 0;
+        description = null;
     }
 
     /**
@@ -20,17 +22,20 @@ public class Item {
      * @param name - name of said item
      * @param value - value of an item for purchase and sell price
      */
-    public Item(String name, int value) {
+    public Item(String name, int value, String description) {
         this.name = name;
         this.value = value;
+        this.description = description;
     }
 
     //getters
     public String getName() {return name;}
     public int getValue() {return value;}
+    public String getDescription() {return description;}
 
     //setters
     public void setName(String name) {this.name = name;}
     public void setValue(int value) {this.value = value;}
+    public void setDescription(String description) {this.description = description;}
 
 }
--- src/RunGame.java
+++ src/RunGame.java
@@ -1,4 +1,9 @@
 public class RunGame {
+    public void load(int difficulty) {
+        //creates each basic object in the game (such as items, weapons, etc.)
+        //contains dialogue
+    }
+
     public static void main(String[] args) {
 
     }
