ed774df5d2eb99d595440d6ca5daed7bc39c16ce
MALENCIASM21 <smalencia99@gmail.com>
2022-11-17 21:15:22 -0500

    made gf special attack

--- src/Girlfriend.java
+++ src/Girlfriend.java
@@ -46,8 +46,17 @@ public class Girlfriend extends Character{
      * Alternate attack specifically for the girlfriend that increases in strength based on its difficulty.
      * Girlfriends have a low chance of using this attack.
      * @param difficulty;
+     * @param player - needed in order to change the Players hp value after being attacked
      */
-    public void specialAttack(int difficulty) {}
+    public void specialAttack(int difficulty, Character player) {
+        System.out.println("I don't know. You've been acting pretty sus lately.");
+        System.out.println("I think we have an imposter Among US");
+        if(difficulty == 1){
+            System.out.println("You have just Scoobied your last Doo");
+            player.setHP(player.getHP() - this.getWeapon().getDamage());
+            System.out.println("Your Girlfriend hits you with the Mystery Machine dealing" + this.getWeapon().getDamage() + " Damage!");
+        }
+    }
 
     /**
      * Returns a string of dialogue from the dialogue list attached to this girlfriend
@@ -55,13 +64,20 @@ public class Girlfriend extends Character{
      */
     public String dialogue() {return "Impress me or die";}
 
+    /**
+     * This method is for when the girlfriend attacks the player
+     * @param opponent - the Character being fought
+     */
     public void fight(Character opponent){
         Random rand = new Random();
         int chc = rand.nextInt(4) + 1;
-        if(chc == 1 && spAtkUsed == false){
-            this.specialAttack(1);
+        if(chc == 1 && !spAtkUsed){
+            this.specialAttack(1, opponent);// change this method for separate difficulties
             spAtkUsed = true;
-        }// change this method for separate difficulties
+        }
+        else{
+            opponent.setHP(opponent.getHP() - this.getWeapon().getDamage());
+        }
 
     }
 
