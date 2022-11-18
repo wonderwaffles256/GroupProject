0844a9c82fd7fc0c7aa78f2d461b894fe2c0a30c
NEWCOMBDE21 <NewcombDE21@GCC.edu>
2022-11-17 20:10:49 -0500

    made a couple stuffs

--- src/RunGame.java
+++ src/RunGame.java
@@ -3,6 +3,12 @@ public class RunGame {
         //creates each basic object in the game (such as items, weapons, etc.)
         //contains dialogue
         Weapon gun = new Weapon("gun", 100, 10, "It shoots stuff");
+        Weapon club = new Weapon("wooden club", 10, 3, "Made of 100% tree");
+
+        Armor overalls = new Armor("overalls", 20, 10, "Ain't much, but it's honest work");
+        Armor nothing = new Armor("none", 0, 0, "Nothing cheaper than a birthday suit");
+
+        //Enemy beet = new Enemy(12, "Baljbeet", )
     }
 
     //use to test runGame
