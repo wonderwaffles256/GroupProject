b2086cc40a4321dae73d61e12f1a37281c1cc895
MALENCIASM21 <smalencia99@gmail.com>
2022-11-28 23:45:27 -0500

    Made a consumable class for healing items

---
+++ src/Consumable.java
@@ -0,0 +1,34 @@
+public class Consumable extends Item{
+    private int healing;
+
+    /**
+     * Default constructor for creating a consumable object
+     */
+    public Consumable() {
+        super();
+        this.healing = 0;
+    }
+
+    /**
+     * constructor for healing object with the data types initialized
+     * @param name name of the object
+     * @param value value of the object
+     * @param healing how much healing the object gives to a player
+     */
+
+    //constructor given input
+    public Consumable(String name, int value, int healing, String description) {
+        super(name, value, description);
+        this.healing = healing;
+    }
+
+    /**
+     * getter for the healing of the object
+     * @return returns healing of the object
+     */
+    //getter / setter
+    public int getHealing() {return healing;}
+
+    public void setHealing(int damage) {this.healing = damage;}
+
+}
