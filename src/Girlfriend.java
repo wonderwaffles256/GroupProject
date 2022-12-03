f8d7b50abe8013c4b32a378a6a74d4a45f9dc82a
MALENCIASM21 <smalencia99@gmail.com>
2022-12-03 17:43:06 -0500

    Some minor fixes

--- src/Girlfriend.java
+++ src/Girlfriend.java
@@ -26,11 +26,10 @@ public class Girlfriend extends Character{
      * @param armor - armor worn by girlfriend
      * @param weapon - weapon wielded by girlfriend
      * @param critChance - chance to land a critical hit
-     * @param medal - award obtained by defeating your girlfriend
      * @param difficulty - level of difficulty related to the girlfriend
      * @param dialogue - list of dialogue
      */
-    public Girlfriend(int HP, String name, Armor armor, Weapon weapon, double critChance, String medal, int difficulty, ArrayList<String> dialogue, ArrayList<String> flirtDialogue, ArrayList<String> flirtResponses, String flirtSuccess) {
+    public Girlfriend(int HP, String name, Armor armor, Weapon weapon, double critChance, int difficulty, ArrayList<String> dialogue, ArrayList<String> flirtDialogue, ArrayList<String> flirtResponses, String flirtSuccess) {
         super(HP, name, armor, weapon, critChance);
         this.medal = makeMedal(difficulty);
         this.difficulty = difficulty;
